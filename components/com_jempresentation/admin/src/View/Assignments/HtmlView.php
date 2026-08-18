2ad98cc6c75b37d345ee78fec02c2432f1d0afcb
Jaap <JKoelman@users.noreply.github.com>
2026-08-19 00:13:57 +0200

    Expose component Options only to core.admin

--- components/com_jempresentation/admin/src/View/Assignments/HtmlView.php
+++ components/com_jempresentation/admin/src/View/Assignments/HtmlView.php
@@ -1,21 +1,43 @@
 <?php
 namespace KoelmanLabs\Component\JemPresentation\Administrator\View\Assignments;
 defined('_JEXEC') or die;
+
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
 use Joomla\CMS\Toolbar\ToolbarHelper;
+
 class HtmlView extends BaseHtmlView
 {
-    public $items = []; public $pagination;
+    public $items = [];
+    public $pagination;
+
     public function display($tpl = null)
     {
-        $items = $this->get('Items'); $this->items = is_array($items) ? $items : []; $this->pagination = $this->get('Pagination');
+        $items = $this->get('Items');
+        $this->items = is_array($items) ? $items : [];
+        $this->pagination = $this->get('Pagination');
+
         $user = Factory::getApplication()->getIdentity();
+
         ToolbarHelper::title(Text::_('COM_JEMPRESENTATION_ASSIGNMENTS'));
-        if ($user->authorise('core.create', 'com_jempresentation')) ToolbarHelper::addNew('assignment.add');
-        if ($user->authorise('core.edit', 'com_jempresentation')) ToolbarHelper::editList('assignment.edit');
-        if ($user->authorise('core.delete', 'com_jempresentation')) ToolbarHelper::deleteList('', 'assignments.delete');
+
+        if ($user->authorise('core.create', 'com_jempresentation')) {
+            ToolbarHelper::addNew('assignment.add');
+        }
+
+        if ($user->authorise('core.edit', 'com_jempresentation')) {
+            ToolbarHelper::editList('assignment.edit');
+        }
+
+        if ($user->authorise('core.delete', 'com_jempresentation')) {
+            ToolbarHelper::deleteList('', 'assignments.delete');
+        }
+
+        if ($user->authorise('core.admin', 'com_jempresentation')) {
+            ToolbarHelper::preferences('com_jempresentation');
+        }
+
         parent::display($tpl);
     }
 }
