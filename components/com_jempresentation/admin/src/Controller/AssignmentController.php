75a3dd34cdccc3ea041d594200362f8aeeb1cc1b
Jaap <JKoelman@users.noreply.github.com>
2026-08-19 00:18:25 +0200

    Require core.manage for assignment mutations

--- components/com_jempresentation/admin/src/Controller/AssignmentController.php
+++ components/com_jempresentation/admin/src/Controller/AssignmentController.php
@@ -1,10 +1,26 @@
 <?php
 namespace KoelmanLabs\Component\JemPresentation\Administrator\Controller;
+
 defined('_JEXEC') or die;
+
 use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\Controller\FormController;
+
 class AssignmentController extends FormController
 {
-    protected function allowAdd($data = []) { return Factory::getApplication()->getIdentity()->authorise('core.create', 'com_jempresentation'); }
-    protected function allowEdit($data = [], $key = 'id') { return Factory::getApplication()->getIdentity()->authorise('core.edit', 'com_jempresentation'); }
+    protected function allowAdd($data = [])
+    {
+        $user = Factory::getApplication()->getIdentity();
+
+        return $user->authorise('core.manage', 'com_jempresentation')
+            && $user->authorise('core.create', 'com_jempresentation');
+    }
+
+    protected function allowEdit($data = [], $key = 'id')
+    {
+        $user = Factory::getApplication()->getIdentity();
+
+        return $user->authorise('core.manage', 'com_jempresentation')
+            && $user->authorise('core.edit', 'com_jempresentation');
+    }
 }
