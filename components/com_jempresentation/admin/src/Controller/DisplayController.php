c3c3773daf3efb586a419cee69b5b1415c9a70b1
Jaap <JKoelman@users.noreply.github.com>
2026-08-19 00:13:48 +0200

    Enforce core.manage on Presentation admin entry

--- components/com_jempresentation/admin/src/Controller/DisplayController.php
+++ components/com_jempresentation/admin/src/Controller/DisplayController.php
@@ -1,8 +1,22 @@
 <?php
 namespace KoelmanLabs\Component\JemPresentation\Administrator\Controller;
+
 defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\Controller\BaseController;
+
 class DisplayController extends BaseController
 {
     protected $default_view = 'assignments';
+
+    public function display($cachable = false, $urlparams = [])
+    {
+        if (!Factory::getApplication()->getIdentity()->authorise('core.manage', 'com_jempresentation')) {
+            throw new \RuntimeException(Text::_('JERROR_ALERTNOAUTHOR'), 403);
+        }
+
+        return parent::display($cachable, $urlparams);
+    }
 }
